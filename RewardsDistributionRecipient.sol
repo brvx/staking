e67627f5f19e04ae4dfb1be0421cdf9386594e19
0x <63167744+brvx@users.noreply.github.com>
2022-04-25 21:38:32 -0700

    Create RewardsDistributionRecipient.sol

---
+++ RewardsDistributionRecipient.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.5.16;
+//SPDX-License-Identifier: MIT
+
+// Inheritance
+import "./Owned.sol";
+
+ abstract contract RewardsDistributionRecipient is Owned {
+    address public rewardsDistribution;
+
+    function notifyRewardAmount(uint256 reward) virtual external;
+
+    modifier onlyRewardsDistribution() {
+        require(msg.sender == rewardsDistribution, "Caller is not RewardsDistribution contract");
+        _;
+    }
+
+    function setRewardsDistribution(address _rewardsDistribution) external onlyOwner {
+        rewardsDistribution = _rewardsDistribution;
+    }
+}
